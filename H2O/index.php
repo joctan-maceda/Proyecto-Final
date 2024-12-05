619d4ef943fc1811d1ff36910cb400a7213838ef
Alejandro Tejeda <alejanafs28@gmail.com>
2024-12-04 20:23:55 -0600

    Se agrego los datos del inicio de la ods

--- H2O/index.php
+++ H2O/index.php
@@ -6,24 +6,76 @@
        
 
         <div class="row w-100" style="padding-top:20px;">
-            <h3>OBJETIVOS DE LA CEASPUE</h3>
+            <h3>AGUA LIMPIA Y SANEAMIENTO</h3>
         </div>
+        <div class="row w-100" style="padding-top:20px;">
+            <p style="text-align: justify; font-size: 18px;"> El acceso al agua potable, al saneamiento y a la higiene es una 
+                necesidad básica y un derecho humano fundamental, vital para la 
+                salud, el bienestar y el desarrollo sostenible. Sin embargo, 
+                miles de millones de personas en todo el mundo aún carecen de 
+                estos servicios esenciales. Se estima que, sin un avance significativo, 
+                para 2030 millones de personas seguirán enfrentándose a esta crisis.
+                La creciente demanda de agua, impulsada por el rápido crecimiento demográfico, 
+                la urbanización y el aumento de necesidades agrícolas, industriales y 
+                energéticas, está superando la disponibilidad de este recurso. Actualmente, 
+                la mitad de la población mundial experimenta escasez severa de agua al 
+                menos un mes al año. Este problema se agrava con el cambio climático, 
+                que intensifica fenómenos como sequías, disminuye la previsibilidad 
+                en la disponibilidad de agua y amenaza la biodiversidad.
+                En 2022, 2,200 millones de personas no tenían acceso a agua 
+                potable gestionada de manera segura, y 3,500 millones carecían
+                 de saneamiento adecuado, exponiéndose a enfermedades como malaria 
+                 y diarrea, y aumentando las tasas de mortalidad.
+                .</p>
+        </div>
+        <div class="column col-md-6">
+                <div class="row w-100 mt-3 justify-content-center">
+                    <img src="./assets/img/ag-lp.jpg" alt="Imagen de Saneamiento"  class="img-fluid w-100" style="padding-bottom:20px;">
+                </div>
+            </div>
+       
 
+
+        <div class="row w-100" style="padding-top:10px;">
+            <h2>PRINCIPALES DESAFIOS</h2>
+      </div>
         <div class="row w-100 mt-4">
         <ul class="list-unstyled custom-list" style="list-style-type: disc; padding-left: 20px; text-align: justify;">
 
-          <li style=" margin-bottom: 10px;">Desarrollar la planeación hídrica a nivel estatal.</li>
-          <li style=" margin-bottom: 10px;">Proyectar, presupuestar y ejecutar, la obra pública, así como la instrumentación de la misma en todas sus etapas en materia de infraestructura hídrica.</li>
-          <li style=" margin-bottom: 10px;">Apoyar a los municipios y organismos operadores, que así lo requieran, en el ejecución de la obra pública y sus servicios relacionados, previa suscripción de Convenios de Colaboración, con el objeto de ampliar la infraestructura hídrica.</li>
-          <li style=" margin-bottom: 10px;">Prestar servicios de apoyo y asistencia técnica en la materia que requieran los municipios y organismos operadores.</li>
-          <li style=" margin-bottom: 10px;">Implementar políticas y planes que tengan como objetivo principal concientizar y fomentar el hábito permanente de la población para el cuidado y uso racional del agua.</li>
-          <li style=" margin-bottom: 10px;">Coadyuvar con los municipios y organismos operadores con la finalidad de desarrollar programas de orientación para la preservación, propiciar el aprovechamiento y calidad del agua.</li>
-
+          <li style=" margin-bottom: 10px;">Escasez de recursos hídricos: La demanda supera la oferta, agravada por el cambio climático.</li>
+          <li style=" margin-bottom: 10px;">Infraestructura insuficiente: Las instalaciones para saneamiento y agua potable son deficientes o inexistentes en muchas áreas.</li>
+          <li style=" margin-bottom: 10px;">Contaminación del agua: La degradación de ecosistemas acuáticos afecta tanto la calidad como la disponibilidad del agua.</li>
+          <li style=" margin-bottom: 10px;">Gestión ineficaz de recursos: La falta de planificación y coordinación intersectorial limita los avances.</li>
+          <li style=" margin-bottom: 10px;">Falta de concienciación y educación: Es esencial aumentar la comprensión sobre el uso eficiente y sostenible del agua.</li>
         </ul>
       </div>
+
+      <div class="column col-md-6">
+                <div class="row w-100 mt-3 justify-content-center">
+                    <img src="./assets/img/san.jpg" alt="Imagen de Saneamiento"  class="img-fluid w-100" style="padding-bottom:20px;">
+                </div>
+            </div>
+       
+
       <div class="row w-100" style="padding-top:10px;">
-            <h2>SANEAMIENTO EN PUEBLA</h2>
+            <h2>¿QUE PRETENDE ESTA PAGINA WEB?</h2>
       </div>
+      <div class="row w-100" style="padding-top:20px;">
+            <p style="text-align: justify; font-size: 18px; padding-bottom:20px"> 
+                En el municipio de Puebla, la calidad del agua es 
+                una preocupación creciente debido a la contaminación 
+                por vertidos en ríos como el Atoyac, sumado al rápido 
+                crecimiento urbano y la escasez del recurso. La falta 
+                de información accesible y confiable dificulta la toma
+                 de decisiones informadas, mientras que la baja conciencia 
+                 ciudadana sobre la magnitud de la crisis hídrica perpetúa 
+                 prácticas insostenibles. Este proyecto busca abordar ambos 
+                 problemas, proporcionando datos actualizados sobre el estado 
+                 del agua y ofreciendo herramientas educativas que promuevan 
+                 un cambio en los hábitos individuales y colectivos, 
+                 contribuyendo así a la conservación y manejo responsable del agua en la región.
+            </p>
+        </div>
 
 
       </div>           
